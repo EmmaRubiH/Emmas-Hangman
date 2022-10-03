e90e7870b81a7cae212fed9aa5b4251c595ee6da
EmmaRubiH <emmahjorting89@gmail.com>
2022-10-03 14:05:09 +0000

    add in codes for right and wrong answers

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+words = ['python', 'program']
+
+picked = random.choice(words)
+
+
+right = []
+wrong = []
+
+while True: 
+
+    print('==================')
+
+    guess = input("Guess a letter")
+
+
+    if guess in picked:
+        right.append(guess)
+        print('Right:' ,right)
+    else:
+        wrong.append(guess)
+        print('Wrong:' ,wrong)
